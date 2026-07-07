25738e8c4c10deedb90ee2eef8ff454089851d7f
Stark2131 <sejal.arbavdhankar@gmail.com>
2026-07-07 22:05:53 +0530

    removed auth as null

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,4 @@
-import { getAuth, type Auth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import app from "./firebase";
 
-export const auth: Auth | null = app ? getAuth(app) : null;
+export const auth = getAuth(app);
